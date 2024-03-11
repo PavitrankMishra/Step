fb1d15cc8c0f9c0e55fba11911546702562d1147
Pavitrank <pavitrank2002@gmail.com>
2024-03-11 20:17:31 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setOpen] = useState(false);
 
-  const messages = ["Learn React", "Apply for Job", "Get the job done"];
+  const messages = ["Learn React®️", "Apply for Job💼", "Get the job done🤑"];
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
   }
   return (
     <>
-      <button className="close" onClick = {() => setOpen(!isOpen)}>&times;</button>
+      <button className="close" onClick={() => setOpen((is) => !is)}>
+        &times;
+      </button>
       {isOpen && (
         <div className="steps">
           <div className="numbers">
